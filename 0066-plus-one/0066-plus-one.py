5de2451499f1c0324926a636b163941c283d5170
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2022-11-26 21:51:17 +0100

    Update 0066-plus-one.py

--- 0066-plus-one/0066-plus-one.py
+++ 0066-plus-one/0066-plus-one.py
@@ -4,7 +4,6 @@ class Solution(object):
         :type digits: List[int]
         :rtype: List[int]
         """
-        #can we google a way to convert list to int during the interview?
         
         strs = [str(digit) for digit in digits]
         str_digits = "".join(strs)
