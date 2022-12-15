54a8999ec4af2657a88a399b3ba897db1c0002c0
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2022-12-15 17:00:46 +0100

    Create 0013-roman-to-integer.py

---
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        hash_table = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
+
+        idx = len(s)-1
+        sum = 0
+        while idx >=0:
+            if idx < len(s)-1 and hash_table[s[idx]] < hash_table[s[idx+1]]:
+                sum -= hash_table[s[idx]]
+            else:
+                sum += hash_table[s[idx]]
+            idx -= 1 
+        return sum
