74b2e58dd551a21122eefbe90615babfea78a287
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2023-08-07 18:32:08 -0400

    Time: 1 ms (77.23%), Space: 41.5 MB (76.02%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.java
@@ -1,13 +1,8 @@
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-        int max = -1;
-        for (int num : candies){
-            if (num > max)
-                max = num;
-        }
+        int max = max(candies);
 
         List<Boolean> result = new ArrayList<Boolean>(candies.length);
-
 
         for (int i = 0; i < candies.length; i++){
             if ((candies[i] + extraCandies) >= max)
@@ -20,15 +15,14 @@ class Solution {
 
     }
 
-    /*private static int max(int[] array){
-        int max = -1;
+    private static int max(int[] array){
+        int max = Integer.MIN_VALUE;
         for (int num : array){
             if (num > max)
                 max = num;
         }
 
-        System.out.print(max);
         return max;
 
-    }*/
+    }
 }
