6f4431a8782ee72b529f84a1e131abd8a4c312a8
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2023-08-30 22:14:40 -0400

    Time: 27 ms (81.21%), Space: 13.2 MB (78.43%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -12,14 +12,19 @@ class Solution(object):
             'C' : 100,
             'D' : 500,
             'M' : 1000
-        }
-
-        idx = len(s)-1
+        } 
+        # 01
+        # IV 
+        idx = len(s) - 1
         sum = 0
-        while idx >=0:
-            if idx < len(s)-1 and hash_table[s[idx]] < hash_table[s[idx+1]]:
+        while idx >= 0:
+            #if (idx > 1 and hash_table[s[idx]] > hash_table[s[idx-1]]):
+            if (idx < len(s) - 1 and hash_table[s[idx]] < hash_table[s[idx+1]]):
                 sum -= hash_table[s[idx]]
             else:
                 sum += hash_table[s[idx]]
-            idx -= 1 
+            idx -= 1
         return sum
+
+
+
