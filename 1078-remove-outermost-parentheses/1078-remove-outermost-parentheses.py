53c90e00b8c22067b2b356a4150a86312c34a254
Ronald Ngounou <74538524+ronaldngounou@users.noreply.github.com>
2023-10-12 00:34:00 -0400

    Time: 52 ms (11.05%), Space: 16.4 MB (34.24%) - LeetHub

--- 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
+++ 1078-remove-outermost-parentheses/1078-remove-outermost-parentheses.py
@@ -2,6 +2,11 @@ class Solution:
     def removeOuterParentheses(self, s: str) -> str:
         stack = []
         l = []
+
+        # Solve this problem using a stack and a list to keep track of the indexes where the outer most parenthesis are.
+        # Remove the corresponding parenthesis from the stack if they match. If they don't match, append the element in the stack.
+        # When the stack is empty, we have reached an outer most parenthesis. Store the index of that outer parenthesis inside a list.
+        
         for i in range(len(s)):
             if not stack:
                 l.append(i)
